db6aad648b60f8181c5c496365ee73f78a169426
Alejandro Perez <alejandroperezcosio@gmail.com>
2021-12-06 10:29:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# ready-python-config
+Simple config module ready to use.
+
+## Setup
+
+```shell
+pip install git+https://github.com/alejandroperezcosio/ready-python-config.git
+```
+
+## Usage
+```bash
+python app.py -env=staging -c=elead
+```
+
+```python
+from rpyconfig import config
+
+print config.* # This is an object
+```
---
+++ rpyconfig/__init__.py
@@ -0,0 +1,61 @@
+import os
+import json
+import argparse
+
+env = os.environ.get('env') or 'default'
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--environment", "-env", help="Environment to use", default=env)
+parser.add_argument("--config", "-c", help="Configuration to use", default="")
+args = parser.parse_args()
+
+
+class DictToObject(object):
+    def __init__(self, dictionary):
+        for key, value in dictionary.items():
+            if type(value) == dict:
+                setattr(self, key, DictToObject(value))
+            else:
+                setattr(self, key, value)
+
+
+class Config:
+    __instance__ = None
+
+    def __init__(self, args):
+        self.args = args
+        self.current = self.get_config()
+        if Config.__instance__ == None:
+            Config.__instance__ = self
+
+
+    def get_config(self):
+        environment = self.args.environment
+        config = "-%s" % self.args.config if self.args.config else ""
+
+        current_dir =  os.path.abspath(os.path.dirname(__file__))
+
+        specific_config_file = os.path.abspath(current_dir + "/../config/%s%s.json" % (environment, config))
+        env_config_file = os.path.abspath(current_dir + "/../config/%s.json" % (environment))
+
+        # Try to use the <env>-<conf> file
+        conf = os.path.exists(specific_config_file) and open(specific_config_file)
+        # If <env>-<conf> file doesn't exist, use the <env> file
+        if not conf: conf = open(env_config_file)
+
+        # Make the json config a nested object
+        configuration = DictToObject(json.load(conf))
+
+        return configuration
+
+
+    @staticmethod
+    def get_instance():
+        result = object
+        if Config.__instance__ != None and hasattr(Config.__instance__, 'current'):
+            result = Config.__instance__.current
+
+        return result
+
+Config(args)
+config = Config.get_instance()
